05e3c4afaec3dc18a56019649553bdfcedacc5dc
Emile Fugulin <emilefugulin@hotmail.com>
2017-01-06 10:20:17 -0500

    Fix formatting

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import time
 
 i = 0
 while True:
-   f = open('output.txt', 'w')
-   f.write('Working: ' + str(i))
-   i += 1
-   time.sleep(1) 
+    f = open('output.txt', 'w')
+    f.write('Working: ' + str(i))
+    i += 1
+    time.sleep(1)
