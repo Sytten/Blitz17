e79068f314a95d59dd8e9bd024a2e459baf8d4b6
Hugo Bedard <Hugo.Bedard@USherbrooke.ca>
2017-01-07 07:22:11 -0500

    Remove ;

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import requests
 
 i = 0
 while True:
-    r = requests.get('http://www.google.com');
+    r = requests.get('http://www.google.com')
     f = open('output.txt', 'w')
     f.write('Working: ' + str(i))
     f.write('\nStatus: ' + str(r.status_code))
